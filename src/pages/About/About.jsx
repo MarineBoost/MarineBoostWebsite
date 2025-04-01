ab0f14c1ab505a49d2e2e05e99f921434687b525
MarineBoost <your-email@example.com>
2025-04-01 15:39:47 +0530

    01/04/2025 commit by amarjeet

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './About.css';
 import { Link } from 'react-router-dom';
-import AboutUsSectionImg from '../../utils/images/about-us-section-img.jpg';
+import AboutUsSectionImg from '../../utils/images/marineboost_white.jpeg';
 import ChooseSection from '../../components/ChooseSection/ChooseSection';
 import Person1 from '../../utils/images/person1.jpg';
 import Person2 from '../../utils/images/person2.jpg';
