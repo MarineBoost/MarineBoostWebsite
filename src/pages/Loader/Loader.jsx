4455fc642c9bdf40c6c3e54060ca55a51198c32c
MarineBoost <your-email@example.com>
2025-04-07 14:56:47 +0530

    06/04/2025 commit by amarjeet

--- src/pages/Loader/Loader.jsx
+++ src/pages/Loader/Loader.jsx
@@ -1,16 +1,45 @@
 // src/components/Loader.js
 import React from "react";
-import { InfinitySpin } from 'react-loader-spinner'; 
+import { ColorRing } from 'react-loader-spinner';
+import logo from "../../utils/images/marineboost.jpeg";
 
 const Loader = () => {
   return (
-    <div className="loader-container" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <InfinitySpin
-        visible={true} 
-        width="200"     
-        color="#4fa94d" 
-        ariaLabel="infinity-spin-loading" 
+    <div
+      className="loader-container"
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+        position: 'relative',
+      }}
+    >
+      {/* Centered Logo */}
+      <img
+        src={logo}
+        alt="Logo"
+        style={{
+          width: '50px',
+          height: '50px',
+          borderRadius: '50%',
+          position: 'absolute',
+          zIndex: 2,
+        }}
       />
+
+      {/* Spinner Around Logo */}
+      <div style={{ zIndex: 1 }}>
+        <ColorRing
+          visible={true}
+          height="150"
+          width="150"
+          ariaLabel="color-ring-loading"
+          wrapperStyle={{}}
+          wrapperClass="color-ring-wrapper"
+          colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+        />
+      </div>
     </div>
   );
 };
